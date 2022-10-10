4fb90bd1579092245d6a3f11250e0c74fdb6fbcf
Danilo <didox_59@yahoo.com.br>
2022-10-10 12:47:21 -0300

    mvc net framework com alunos stone

---
+++ WebMVC/Controllers/AnimaisController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebMVC.Models;
+using WebMVC.Servicos;
+
+namespace WebMVC.Controllers
+{
+    public class AnimaisController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Cadastrar(string nome)
+        {
+            AnimalServico.Instancia().Add(new Animal() { 
+                Nome = nome
+            });
+
+            ViewBag.mensagem = "Cadastrado com sucesso";
+            return View();
+        }
+    }
+}
